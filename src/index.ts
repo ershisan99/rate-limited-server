5551086ed4de3a3e64a891a2e9d69ee359d6fa6b
andres <andres99.dev@gmail.com>
2024-08-20 19:15:38 +0200

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
-import { Elysia } from "elysia";
+import {Elysia} from "elysia";
+import {rateLimit} from "elysia-rate-limit";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const url =
+    "https://script.google.com/macros/s/AKfycbwHGQjf1vlKbY7UJ3h1OUTywJCgma6SlJi94WXaFBhS_93cdfHhIB0mVQBHqHRtbO4dQQ/exec";
+
+
+const app = new Elysia()
+    .use(rateLimit({
+        max: 2
+    }))
+
+    .post("/", async ({body}) => {
+        const response = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers: {"Content-Type": "application/json"},
+        })
+
+
+        return await response.json()
+    })
+
+    .listen(process.env.PORT ?? 4444);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
